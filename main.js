546986da2e2ae9e94c2f10fae40a2b19107249df
Sérgio Nunes <sagnunes@gmail.com>
2024-10-15 09:54:10 +0100

    removed tailwindcss dependecies added all inline styles to email template fixed some bug when sending the emails

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
-    secure: false,
+    secure: 'null',
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
@@ -26,7 +26,7 @@ async function getTickets() {
     try {
 
         await connection.connect();
-        const [rows] = await connection.query(`SELECT * FROM view WHERE closed BETWEEN '2024-10-14 00:00:00' AND '2024-10-14 23:59:59'`);
+        const [rows] = await connection.promise().query(`SELECT * FROM view_closed_tickets WHERE closed BETWEEN '2024-10-14 00:00:00' AND '2024-10-14 23:59:59'`);
         return rows
     } catch (error) {
         console.log(error);
@@ -35,31 +35,34 @@ async function getTickets() {
 
 async function sendEmails(records) {
 
-    ejs.renderFile(__dirname + '/views/emails.ejs', {records}, function (err, template) {
+    ejs.renderFile(__dirname + '/src/views/emails.ejs', {records: records}, async function (err, template) {
         if (err) {
             console.log(err);
         } else {
             for (const ticket of records) {
 
+                console.log(ticket)
                 const mailOptions = {
                     from: process.env.MAIL_DEPARTMENT,
-                    to: ticket.address,
+                    to: 'sergio.ag.nunes@madeira.gov.pt',
                     subject: ticket.subject,
                     html: template
                 };
 
-                transporter.sendMail(mailOptions, function (err, info) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(info);
-                        console.log('Message send successfully.');
-                    }
-                });
+                try {
+                    await transporter.sendMail(mailOptions);
+                    console.log('Email enviado com sucesso.');
+                } catch (error) {
+                    console.error('Erro ao enviar o email:', error);
+                }
             }
         }
     })
 }
 
-let records = await getTickets();
-sendEmails(records).then(r => console.log(r));
+async function main() {
+    let records = await getTickets();
+    await sendEmails(records);
+}
+
+main();
