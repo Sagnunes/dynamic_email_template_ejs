eef7f28802fb7ce99c0b2fd76f66f1dee4c2f233
Sérgio Nunes <sagnunes@gmail.com>
2024-10-16 15:30:18 +0100

    added loggin system

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const database = require('mysql2');
 const ejs = require('ejs');
 const moment = require('moment');
+const logger = require('pino')();
 
 const connection = database.createConnection({
     host: process.env.DB_HOST,
@@ -9,23 +10,19 @@ const connection = database.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
-
-
 const nodemailer = require('nodemailer');
 
+const email_not_send = 2
+const email_send = 1
+
 const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: false,
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASSWORD,
+    host: process.env.MAIL_HOST, port: process.env.MAIL_PORT, secure: false, auth: {
+        user: process.env.MAIL_USER, pass: process.env.MAIL_PASSWORD,
     }
 });
 
 async function getTickets() {
-    // let actualDate = moment().format('YYYY-MM-DD');
-    let actualDate = '2024-10-14'
+    let actualDate = moment().format('YYYY-MM-DD');
     try {
         await connection.connect();
         const [rows] = await connection.promise().query(`SELECT * FROM view_closed_tickets WHERE closed BETWEEN '${actualDate} 00:00:00' AND '${actualDate} 23:59:59'`);
@@ -36,20 +33,18 @@ async function getTickets() {
 }
 
 async function sendEmails(records) {
-
-    if (!records) {
-        console.log('no records')
+    if (records.length < 1) {
+        logger.info('No Records Found');
     } else {
         for (const ticket of records) {
-
             ticket.createdAt = moment(ticket.created).format('DD-MM-YYYY')
             ejs.renderFile(__dirname + '/src/views/emails.ejs', {ticket}, async function (err, template) {
                 if (err) {
-                    console.log(err);
+                    logger.error(`Template can't be loaded`)
                 } else {
                     const mailOptions = {
                         from: process.env.MAIL_DEPARTMENT,
-                        to: 'corina.lf.gomes@madeira.gov.pt',
+                        to: 'sergio.ag.nunes@madeira.gov.pt',
                         subject: 'Inquérito de satisfação do Ticket #' + ticket.ticket_number,
                         html: template,
                         attachments: [{
@@ -58,12 +53,20 @@ async function sendEmails(records) {
                             cid: 'logo'
                         }]
                     };
-
                     try {
+                        let actualDate = moment().format('YYYY-MM-DD');
+                        const sql = 'INSERT INTO `ost_notification_emails`(`email`, `ticket`, `subject`, `status`, `createdAt`) VALUES (?, ?, ?, ?, ?)';
+                        const values = [ticket.address, ticket.ticket_number, ticket.subject, email_send, actualDate];
                         await transporter.sendMail(mailOptions);
-                        console.log('Email enviado com sucesso.');
+                        await connection.execute({sql, values});
+                        logger.info(`Email has send to - ${ticket.address}`)
+
                     } catch (error) {
-                        console.error('Erro ao enviar o email:', error);
+                        let actualDate = moment().format('YYYY-MM-DD');
+                        const sql = 'INSERT INTO `ost_notification_emails`(`email`, `ticket`, `subject`, `status`, `createdAt`) VALUES (?, ?, ?, ?, ?)';
+                        const values = [ticket.address, ticket.ticket_number, ticket.subject, email_not_send, actualDate];
+                        await connection.execute({sql, values});
+                        logger.error(`Error sending email to - ${ticket.address}`)
                     }
                 }
             })
@@ -78,7 +81,7 @@ async function main() {
 
     setTimeout(() => {
         process.exit(0);
-    }, 5 * 60 * 1000)
+    }, 50000)
 }
 
 main();
